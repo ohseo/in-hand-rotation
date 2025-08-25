915ea95635326b48dda976fabaaa782a04e58f0b
ohseo <seoyoung.oh@gmail.com>
2025-08-25 17:20:30 +0900

    rename variables of scene manager.

--- Assets/Scripts/EvaluationSceneManager.cs
+++ Assets/Scripts/EvaluationSceneManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EvaluationSceneManager : MonoBehaviour
 {
@@ -8,20 +9,29 @@ public class EvaluationSceneManager : MonoBehaviour
     private RotationInteractor _rotationInteractor;
     [SerializeField]
     private GameObject _diePrefab, _targetPrefab;
+    [SerializeField]
+    private int _expCondition = 0; // 0: Baseline, 1: Linear, 2: Power, 3: Tanh
+    [SerializeField]
+    private bool _isLeftHanded = false;
+    [SerializeField]
+    protected TextMeshProUGUI _text;
+
     private GameObject _die, _target;
-    private float _cubeScale = 0.04f;
-    private float _initRotationDeg = 135f;
+    private const float CUBE_SCALE = 0.04f;
+    private const float INIT_ROTATION_DEG = 135f;
     private Vector3 _initPosition = new Vector3(0.1f, 1.1f, 0.3f);
     private Vector3 _posError;
     private Quaternion _rotError;
-    private float _posThreshold = 0.003f, _rotThresholdDeg = 5f;
-    private bool _isTaskComplete = false;
-    [SerializeField]
-    private int _transferFunction;
-    private int _trialNum;
-    private float _dwellThreshold = 1f;
-    private float _dwellTime;
-    private bool _isDwelling = false;
+    private const float POSITION_THRESHOLD = 0.003f, ROTATION_THRESHOLD_DEG = 180f;
+
+    private bool _isDwelling = false, _isTaskComplete = false, _isTimeout = false, _isInTrial = false, _isInSet = false;
+
+    private const float DWELL_THRESHOLD = 1f, TIMEOUT_THRESHOLD = 20f;
+    private float _dwellDuration, _trialDuration;
+
+    private const int MAX_TRIAL_NUM = 15;
+    private int _trialNum = 1;
+
     
 
     void Awake()
@@ -75,14 +85,14 @@ public class EvaluationSceneManager : MonoBehaviour
         float pError = deltaPos.magnitude;
         deltaRot.ToAngleAxis(out float rError, out Vector3 axis);
         // Debug.Log(string.Format("Error: {0}, {1}",pError, rError));
-        return (pError < _posThreshold) && ((rError < _rotThresholdDeg) || (rError > 360f - _rotThresholdDeg));
+        return (pError < POSITION_THRESHOLD) && ((rError < ROTATION_THRESHOLD_DEG) || (rError > 360f - ROTATION_THRESHOLD_DEG));
     }
 
     private void GenerateDie()
     {
         _die = Instantiate(_diePrefab);
         _die.transform.position = new Vector3(-_initPosition.x, _initPosition.y, _initPosition.z);
-        _die.transform.localScale = new Vector3(_cubeScale, _cubeScale, _cubeScale);
+        _die.transform.localScale = new Vector3(CUBE_SCALE, CUBE_SCALE, CUBE_SCALE);
         _die.transform.rotation = Quaternion.identity;
     }
 
@@ -94,7 +104,7 @@ public class EvaluationSceneManager : MonoBehaviour
     private void ResetDie()
     {
         _die.transform.position = new Vector3(-_initPosition.x, _initPosition.y, _initPosition.z);
-        _die.transform.localScale = new Vector3(_cubeScale, _cubeScale, _cubeScale);
+        _die.transform.localScale = new Vector3(CUBE_SCALE, CUBE_SCALE, CUBE_SCALE);
         _die.transform.rotation = Quaternion.identity;
     }
 
@@ -102,9 +112,9 @@ public class EvaluationSceneManager : MonoBehaviour
     {
         _target = Instantiate(_targetPrefab);
         Vector3 axis = Random.onUnitSphere;
-        _target.transform.Rotate(axis.normalized, _initRotationDeg);
+        _target.transform.Rotate(axis.normalized, INIT_ROTATION_DEG);
         _target.transform.position = _initPosition;
-        _target.transform.localScale = new Vector3(_cubeScale, _cubeScale, _cubeScale);
+        _target.transform.localScale = new Vector3(CUBE_SCALE, CUBE_SCALE, CUBE_SCALE);
     }
 
     private void DestroyTarget()
